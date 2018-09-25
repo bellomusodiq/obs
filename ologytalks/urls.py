e12b44928e62619bbb8c20d83830239d5429c948
bmayowa25 <bmayowa25@gmail.com>
2018-09-25 13:25:45 +0100

    finaly

--- ologytalks/urls.py
+++ ologytalks/urls.py
@@ -27,6 +27,7 @@ router.register(r'posts', PostViewSet, base_name='posts')
 router.register(r'category', CategoryViewSet, base_name='category')
 router.register(r'comments', CommentViewSet, base_name='comments')
 from django.views.generic import TemplateView
+from django.views.static import serve
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -41,9 +42,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/account/comment/', CommentPost.as_view() ),
     path('api-token-auth/', obtain_jwt_token),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += re_path('\b(?:/media/).*', TemplateView.as_view(template_name='index.html')),
+urlpatterns += re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
     
