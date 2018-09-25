accdaf3bd239cb591e5a3ecf45c3579b474617b9
bmayowa25 <bmayowa25@gmail.com>
2018-09-25 10:59:13 +0100

    final

--- ologytalks/urls.py
+++ ologytalks/urls.py
@@ -45,5 +45,5 @@ from django.conf.urls.static import static
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# urlpatterns += re_path('.*', TemplateView.as_view(template_name='index.html')),
+urlpatterns += re_path('.*', TemplateView.as_view(template_name='index.html')),
     
