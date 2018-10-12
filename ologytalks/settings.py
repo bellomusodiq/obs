4572695ff63e0f5e640dbc29b289f7353d3c0eb9
bmayowa25 <bmayowa25@gmail.com>
2018-10-12 18:52:00 +0100

    new

--- ologytalks/settings.py
+++ ologytalks/settings.py
@@ -160,6 +160,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
 }
 from django.conf import settings
 import datetime
